f08b4a21394bac8666731992021bd3f4bb396585
princesinghrajput <psr8084@gmail.com>
2026-02-13 03:54:52 +0530

    fix: light & dark theme for map

--- frontend/src/components/RouteMap.jsx
+++ frontend/src/components/RouteMap.jsx
@@ -7,8 +7,8 @@ import { renderToStaticMarkup } from 'react-dom/server';
 import { useTheme } from '@/hooks/useTheme';
 
 const TILES = {
-    dark: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
-    light: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
+    dark: 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+    light: 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
 };
 
 // ── Icon Factory ──
